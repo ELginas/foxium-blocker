a9dc068b3d2a479475e8c6fc64bbd166beb31ccd
ELginas <gintarasg909@gmail.com>
2023-07-01 17:31:10 +0300

    complete google destruction

--- background.js
+++ background.js
@@ -2,20 +2,37 @@ function listener(details) {
     let filter = browser.webRequest.filterResponseData(details.requestId);
     let decoder = new TextDecoder("utf-8");
     let encoder = new TextEncoder();
-  
+
     filter.ondata = (event) => {
-      let str = decoder.decode(event.data, {stream: true});
-      str = "<style>#xe7COe{display: none !important;}body{overflow: visible !important;}</style>" + str
-      filter.write(encoder.encode(str));
-      filter.disconnect();
+        let str = decoder.decode(event.data, { stream: true });
+        const prefix = '<!doctype html>'
+
+        str =
+            "<!doctype html>" +
+            "<style>#xe7COe{display: none !important;}body{overflow: visible !important;}</style>" +
+            "<script nonce=\"FhZ-vO7FbyxG9sWPTIcHiQ\">window.addEventListener('load',()=>document.querySelector('.QS5gu.sy4vM')?.click())</script>" +
+            str.slice(prefix.length)
+
+        filter.write(encoder.encode(str));
+        filter.disconnect();
     }
-  
+
     return {};
-  }
-  
-  browser.webRequest.onBeforeRequest.addListener(
+}
+
+function changeHeaders(e) {
+    const csp = e.responseHeaders.filter(header => header.name == 'content-security-policy')[0]
+    csp.value = csp.value.replace('script-src', "script-src 'nonce-FhZ-vO7FbyxG9sWPTIcHiQ'")
+    return { responseHeaders: e.responseHeaders }
+}
+
+browser.webRequest.onBeforeRequest.addListener(
     listener,
-    {urls: ["https://*.google.com/*"], types: ["main_frame"]},
+    { urls: ["https://*.google.com/*"], types: ["main_frame"] },
     ["blocking"]
-  );
-  
+);
+browser.webRequest.onHeadersReceived.addListener(
+    changeHeaders,
+    { urls: ["https://*.google.com/*"], types: ["main_frame"] },
+    ["blocking", "responseHeaders"]
+)
